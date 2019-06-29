1652f474fc89eb9bba826666f0b4b3856b5bd91f
TomohikoToyama <golden.firework.launcher@gmail.com>
2019-06-29 18:19:06 +0900

    ソースコード修正

--- Assets/Scenes/nanaki/Scripts/GameManager.cs
+++ Assets/Scenes/nanaki/Scripts/GameManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    private AudioSource bgm;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bgm = GameObject.Find("Audio").GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,9 +23,10 @@ public class GameManager : MonoBehaviour
     {
         if (flg)
         {
+            bgm.Stop();
 
-
-        }else if (!flg)
+        }
+        else if (!flg)
         {
 
         }
