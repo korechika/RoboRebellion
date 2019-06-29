af56dae49a612b49bfc2909d47f496cc1403e849
TomohikoToyama <golden.firework.launcher@gmail.com>
2019-06-29 14:50:52 +0900

    簡易シーン作成

---
+++ Assets/Scenes/MultiDeviceConnection/nanaki/Scripts/ServerPlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerPlayerController : MonoBehaviour
+{
+    private GameObject RoboAvater;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RoboAvater = GameObject.Find("Robot Kyle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 avaterPos = new Vector3(this.transform.position.x, 0, this.transform.position.z);
+        RoboAvater.transform.position = avaterPos;
+        var moveDir = this.transform.rotation.eulerAngles.y;
+        var moveQ = Quaternion.Euler(0, moveDir, 0);
+        RoboAvater.transform.rotation = moveQ;
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
